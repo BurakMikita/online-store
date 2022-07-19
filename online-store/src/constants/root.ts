d88828d641ab85c2e3e7c9b948ed4f2c4b5b701a
BurakMikita <burak.nikita@email.com>
2022-07-19 17:50:45 +0300

    add new projekt

--- online-store/src/constants/root.ts
+++
@@ -1,9 +0,0 @@
-import { target } from 'nouislider';
-export const ROOT_HEADER = document.getElementById('header');
-export const ROOT_SHOPPING = document.getElementById('shopping');
-export const SEARCH_NAME = document.getElementById('search_name');
-export const ERROR = document.getElementById('Error');
-export const ROOT_PRODUCTS = document.getElementById('products');
-export const SORT__ITEM = document.getElementById('sort__item');
-export const SLIDER = <target>document.getElementById('slider');
-export const SLIDER__OLD = <target>document.getElementById('slider__old');
